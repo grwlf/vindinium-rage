282e95c9dbbd15892d7eded47bfd9b159c5b5eaa
Sergey Mironov <grrwlf@gmail.com>
2017-11-27 08:00:00 +0300

    zz, add replay mode

--- bot-zapzap/app/Main.hs
+++ bot-zapzap/app/Main.hs
@@ -8,40 +8,63 @@ import System.Exit
 import Imports
 import Types
 import Driver
-import Args
 import Brain
 import Cli
 
+
+data Args = Args {
+    args_ds :: DriverSettings
+  , args_replay :: String
+  } deriving(Show)
+
 argsParser :: Parser Args
-argsParser = Args
-  <$> option str (long "tag" <> value "")
-  <*> (read <$> option str (long "training" <> short 't' <> value "0"))
-  <*> switch (long "dump-state")
-  <*> switch (long "dump-game")
-  <*> switch (long "quiet" <> short 'q')
+argsParser =
+  Args
+    <$> (
+    DriverSettings
+      <$> option str (long "tag" <> value "")
+      <*> (read <$> option str (long "training" <> short 't' <> value "0"))
+      <*> switch (long "dump-state")
+      <*> switch (long "dump-game")
+      <*> switch (long "quiet" <> short 'q'))
+    <*> (
+    option str (long "replay" <> short 'r' <> value ""))
 
 getArgs = execParser (info (argsParser <**> helper) idm)
 
 main :: IO ()
 main = do
 
-  args@Args{..} <- getArgs
+  Args{..} <- getArgs
+  DriverSettings{..} <- pure args_ds
 
-  out [ "Starting Vindinium bot, training:", tshow args_training,
-        "Tag", tpack args_tag
-      ]
+  case not (null args_replay) of
 
-  driver_net (Key "vhkdc75e") args $ do
+    True -> do
 
-    controller_threaded warmupIO $ \bs g hid chan -> do
+      out [ "Starting replay mode" ]
 
-      moveIO bs g hid chan
+      drawGameFinder args_replay Nothing $ \ss -> do
+        out ["DEBUG HERE"]
+        return ()
 
-      when (not args_quiet) $ do
-        clearTerminal
-        out [ "Tag:", "'" <> tpack args_tag <> "'" ]
-        out [ "Hero:", g.>gameHeroes.(idx hid).heroName, "(" <> printHero hid <> ")" ]
-        blankLine
-        out [ drawGame g [] ]
-        out [ printHeroStats g ]
+    False -> do
+
+      out [ "Starting Vindinium bot, training:", tshow ds_training,
+            "Tag", tpack ds_tag
+          ]
+
+      driver_net (Key "vhkdc75e") args_ds $ do
+
+        controller_threaded warmupIO $ \bs g hid chan -> do
+
+          moveIO bs g hid chan
+
+          when (not ds_quiet) $ do
+            clearTerminal
+            out [ "Tag:", "'" <> tpack ds_tag <> "'" ]
+            out [ "Hero:", g.>gameHeroes.(idx hid).heroName, "(" <> printHero hid <> ")" ]
+            blankLine
+            out [ drawGame g [] ]
+            out [ printHeroStats g ]
 
